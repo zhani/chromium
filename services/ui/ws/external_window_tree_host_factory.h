270be9640a143800e744d19791fdc4e60cf9abde
Antonio Gomes <tonikitoo@igalia.com>
2017-03-22 16:00:18 -0400

    Pass aura window 'properties' from client to server
    Commit allows "properties" to be passed from the client (Aura)
    to the server (MUS).
    
    Today only the "initial bounds" property is checked, but more
    will be in follow up commits (hide decoration, visibility state, etc).
    
    fixup! Pass aura window 'properties' from client to server

--- services/ui/ws/external_window_tree_host_factory.h
+++ services/ui/ws/external_window_tree_host_factory.h
@@ -25,9 +25,13 @@ class ExternalWindowTreeHostFactory
   void AddBinding(mojom::ExternalWindowTreeHostFactoryRequest request);
 
  private:
-  // mojom::WindowTreeHostFactory implementation.
-  void CreatePlatformWindow(mojom::WindowTreeHostRequest tree_host_request,
-                            Id client_id) override;
+  using TransportProperties =
+      std::unordered_map<std::string, std::vector<uint8_t>>;
+
+  void CreatePlatformWindow(
+      mojom::WindowTreeHostRequest tree_host_request,
+      Id transport_window_id,
+      const TransportProperties& transport_properties) override;
 
   WindowServer* window_server_;
   mojo::BindingSet<mojom::ExternalWindowTreeHostFactory> bindings_;
