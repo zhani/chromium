3bd8e92ae53b92e82d6acb069c4ff35a79a24cf4
Frédéric Wang <fwang@igalia.com>
2017-03-22 14:56:17 +0100

    Make mus_demo_unittests work with multiple root windows
    WindowServerTestImpl::EnsureClientHasDrawnWindow verifies whether one
    root window of the WindowTree is drawn. This currently works well for
    mus_demo_unittests because the WindowServer only has a single root
    window. However, MusDemo will open multiple root windows in external
    mode and hence mus_demo_unittests should really check whether all these
    windows are drawn.
    
    BUG=666958
    
    Patch from issue 2715533005 at patchset 80001 (https://codereview.chromium.org/2712203002#ps80001)

--- services/ui/ws/window_server_test_impl.cc
+++ services/ui/ws/window_server_test_impl.cc
@@ -19,27 +19,43 @@ WindowServerTestImpl::~WindowServerTestImpl() {}
 
 void WindowServerTestImpl::OnWindowPaint(
     const std::string& name,
-    const EnsureClientHasDrawnWindowCallback& cb,
+    const EnsureClientHasDrawnRootWindowsCallback& cb,
     ServerWindow* window) {
   WindowTree* tree = window_server_->GetTreeWithClientName(name);
   if (!tree)
     return;
   if (tree->HasRoot(window) && window->has_created_compositor_frame_sink()) {
-    cb.Run(true);
-    window_server_->SetPaintCallback(base::Callback<void(ServerWindow*)>());
+    painted_window_roots_[name]++;
+    if (painted_window_roots_[name] == tree->roots().size()) {
+      painted_window_roots_.erase(name);
+      cb.Run(tree->roots().size());
+      window_server_->SetPaintCallback(base::Callback<void(ServerWindow*)>());
+    }
   }
 }
 
-void WindowServerTestImpl::EnsureClientHasDrawnWindow(
+void WindowServerTestImpl::EnsureClientHasDrawnRootWindows(
     const std::string& client_name,
-    const EnsureClientHasDrawnWindowCallback& callback) {
+    const EnsureClientHasDrawnRootWindowsCallback& callback) {
+  if (painted_window_roots_.find(client_name) != painted_window_roots_.end()) {
+    LOG(ERROR) << "EnsureClientHasDrawnRootWindows is already being executed "
+                  "for that client name.";
+    callback.Run(0);
+    return;
+  }
+
+  painted_window_roots_[client_name] = 0;
   WindowTree* tree = window_server_->GetTreeWithClientName(client_name);
   if (tree) {
     for (const ServerWindow* window : tree->roots()) {
       if (window->has_created_compositor_frame_sink()) {
-        callback.Run(true);
-        return;
+        painted_window_roots_[client_name]++;
       }
+    }
+    if (painted_window_roots_[client_name] == tree->roots().size()) {
+      painted_window_roots_.erase(client_name);
+      callback.Run(tree->roots().size());
+      return;
     }
   }
 
