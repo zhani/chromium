10e083498be11960476404a1529dbcf916ebdf8c
Antonio Gomes <tonikitoo@igalia.com>
2017-03-22 16:31:28 -0400

    Add basic mouse events support
    This CL adds basic mouse events support. Events over overlapping
    areas of multiple Chrome windows is still broken, but should be
    fixed in a follow up CL.

--- services/ui/ws/display.h
+++ services/ui/ws/display.h
@@ -41,6 +41,7 @@ class DisplayBinding;
 class DisplayManager;
 class FocusController;
 class WindowManagerDisplayRoot;
+class WindowManagerState;
 class WindowServer;
 class WindowTree;
 
@@ -237,8 +238,6 @@ class Display : public PlatformDisplayDelegate,
   viz::LocalSurfaceIdAllocator allocator_;
 
   WindowManagerDisplayRootMap window_manager_display_root_map_;
-
-  std::unique_ptr<WindowManagerDisplayRoot> external_mode_root_;
 
   DISALLOW_COPY_AND_ASSIGN(Display);
 };
