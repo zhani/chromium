32596e5f9e6324ca4762b83c723b35c5065a169e
Antonio Gomes <tonikitoo@igalia.com>
2017-04-12 10:17:12 -0400

    Make keyboard minimally functional for non-chrome chrome/mus
    CL passes a TextInputClientPtr mojo handle to SimpleInputMethod
    and uses it to forward handling of basic key events.
    
    Note that we should discuss with @moshayedi what the best way
    is to handle non-chromeos mus. Email thread started offline.
    
    TEST=Basic input works, as well as modifiers, and overall keystrokes
    (home, end, enter, delete, backspace, etc).
    
    Issue #26

--- chrome/browser/ui/views/ime_driver/simple_input_method.cc
+++ chrome/browser/ui/views/ime_driver/simple_input_method.cc
@@ -6,7 +6,8 @@
 
 #include <utility>
 
-SimpleInputMethod::SimpleInputMethod() {}
+SimpleInputMethod::SimpleInputMethod(ui::mojom::TextInputClientPtr client)
+    : client_(std::move(client)) {}
 
 SimpleInputMethod::~SimpleInputMethod() {}
 
@@ -15,8 +16,16 @@ void SimpleInputMethod::OnTextInputTypeChanged(
 
 void SimpleInputMethod::OnCaretBoundsChanged(const gfx::Rect& caret_bounds) {}
 
-void SimpleInputMethod::ProcessKeyEvent(std::unique_ptr<ui::Event> key_event,
+void SimpleInputMethod::ProcessKeyEvent(std::unique_ptr<ui::Event> event,
                                         ProcessKeyEventCallback callback) {
+  DCHECK(event->type() == ui::ET_KEY_PRESSED ||
+         event->type() == ui::ET_KEY_RELEASED);
+  DCHECK(event->IsKeyEvent());
+
+  ui::KeyEvent* key_event = event->AsKeyEvent();
+  if (!key_event->is_char() && key_event->type() == ui::ET_KEY_PRESSED)
+    client_->InsertChar(std::move(event));
+
   std::move(callback).Run(false);
 }
 
