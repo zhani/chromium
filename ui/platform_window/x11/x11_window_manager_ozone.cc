8f811c6a4c889b50a51276b9fc35226ab21068bf
Maksim Sisov <msisov@igalia.com>
2017-05-03 17:09:06 +0300

    Resolve event clicking locations in X11
    This patch fixes capturing and clicking events' location:
      Once event comes for a foreground window, it's event location
      should be adapted using background and foreground bounds.

--- ui/platform_window/x11/x11_window_manager_ozone.cc
+++ ui/platform_window/x11/x11_window_manager_ozone.cc
@@ -8,20 +8,49 @@
 
 namespace ui {
 
-X11WindowManagerOzone::X11WindowManagerOzone() : event_grabber_(None) {}
+X11WindowManagerOzone::X11WindowManagerOzone() : event_grabber_(nullptr) {}
 
-X11WindowManagerOzone::~X11WindowManagerOzone() {}
-
-void X11WindowManagerOzone::GrabEvents(XID xwindow) {
-  if (event_grabber_ != None)
-    return;
-  event_grabber_ = xwindow;
+X11WindowManagerOzone::~X11WindowManagerOzone() {
+  DCHECK(x11_windows_.empty());
 }
 
-void X11WindowManagerOzone::UngrabEvents(XID xwindow) {
-  if (event_grabber_ != xwindow)
+void X11WindowManagerOzone::GrabEvents(X11WindowOzone* window) {
+  if (event_grabber_ == window)
     return;
-  event_grabber_ = None;
+
+  X11WindowOzone* old_grabber = event_grabber_;
+  if (old_grabber)
+    old_grabber->OnLostCapture();
+
+  event_grabber_ = window;
+}
+
+void X11WindowManagerOzone::UngrabEvents(X11WindowOzone* window) {
+  if (event_grabber_ != window)
+    return;
+  event_grabber_->OnLostCapture();
+  event_grabber_ = nullptr;
+}
+
+void X11WindowManagerOzone::AddX11Window(X11WindowOzone* window) {
+  auto it = std::find(x11_windows_.begin(), x11_windows_.end(), window);
+  DCHECK(it == x11_windows_.end());
+  x11_windows_.push_back(window);
+}
+
+void X11WindowManagerOzone::DeleteX11Window(X11WindowOzone* window) {
+  auto it = std::find(x11_windows_.begin(), x11_windows_.end(), window);
+  if (it != x11_windows_.end())
+    x11_windows_.erase(it);
+}
+
+X11WindowOzone* X11WindowManagerOzone::GetX11WindowByTarget(
+    const XID& xwindow) {
+  for (auto* x11_window : x11_windows_) {
+    if (x11_window->xwindow() == xwindow)
+      return x11_window;
+  }
+  return nullptr;
 }
 
 }  // namespace ui
