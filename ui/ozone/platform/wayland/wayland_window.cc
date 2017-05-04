3689fffa515ace6611ad35e6255f8f0a065d95e8
Maksim Sisov <msisov@igalia.com>
2017-05-04 16:34:52 +0300

    [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    This adaptation makes it possible to show/hide and place menu windows
    properly in regards to their parent surface. What is more, this patch
    fixes event locations sent to the client from wayland.
    Conflicts:
    	ui/ozone/platform/wayland/wayland_window.cc
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    This resolves crash when a user requests to open a submenu window
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Typo fix
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)

--- ui/ozone/platform/wayland/wayland_window.cc
+++ ui/ozone/platform/wayland/wayland_window.cc
@@ -15,6 +15,24 @@
 
 namespace ui {
 
+namespace {
+
+static WaylandWindow* g_current_capture_ = nullptr;
+
+static const xdg_popup_listener xdg_popup_listener = {
+    &WaylandWindow::PopupDone,
+};
+
+// TODO(msisov, tonikitoo): fix customization according to screen resolution
+// once we are able to get global coordinates of wayland windows.
+gfx::Rect TranslateBoundsToScreenCoordinates(const gfx::Rect& child_bounds,
+                                            const gfx::Rect& parent_bounds) {
+  int x = child_bounds.x() - parent_bounds.x();
+  int y = child_bounds.y() - parent_bounds.y();
+  return gfx::Rect(gfx::Point(x, y), child_bounds.size());
+}
+}
+
 WaylandWindow::WaylandWindow(PlatformWindowDelegate* delegate,
                              WaylandConnection* connection,
                              const gfx::Rect& bounds)
@@ -34,10 +52,6 @@ WaylandWindow* WaylandWindow::FromSurface(wl_surface* surface) {
 }
 
 bool WaylandWindow::Initialize() {
-  static const xdg_surface_listener xdg_surface_listener = {
-      &WaylandWindow::Configure, &WaylandWindow::Close,
-  };
-
   surface_.reset(wl_compositor_create_surface(connection_->compositor()));
   if (!surface_) {
     LOG(ERROR) << "Failed to create wl_surface";
@@ -51,6 +65,9 @@ bool WaylandWindow::Initialize() {
   ui::mojom::WindowType window_type = ui::mojom::WindowType::WINDOW;
   delegate_->GetWindowType(&window_type);
   if (window_type == ui::mojom::WindowType::WINDOW) {
+    static const xdg_surface_listener xdg_surface_listener = {
+        &WaylandWindow::Configure, &WaylandWindow::Close,
+    };
     xdg_surface_.reset(
         xdg_shell_get_xdg_surface(connection_->shell(), surface_.get()));
     if (!xdg_surface_) {
@@ -59,7 +76,18 @@ bool WaylandWindow::Initialize() {
     }
     xdg_surface_add_listener(xdg_surface_.get(), &xdg_surface_listener, this);
   } else {
-    return false;
+    parent_window_ = connection_->GetCurrentFocusedWindow();
+    if (!parent_window_) {
+      LOG(ERROR) << "Failed to get parent window";
+      return false;
+    }
+
+    CreatePopupWindow();
+    if (!xdg_popup_) {
+      LOG(ERROR) << "Failed to create xdg_popup";
+      return false;
+    }
+    parent_window_->set_child_window(this);
   }
   connection_->ScheduleFlush();
 
@@ -81,10 +109,34 @@ void WaylandWindow::ApplyPendingBounds() {
   connection_->ScheduleFlush();
 }
 
-void WaylandWindow::Show() {}
+void WaylandWindow::CreatePopupWindow() {
+  DCHECK(parent_window_);
+  gfx::Rect bounds =
+      TranslateBoundsToScreenCoordinates(bounds_, parent_window_->GetBounds());
+  xdg_popup_.reset(xdg_shell_get_xdg_popup(
+      connection_->shell(), surface_.get(), parent_window_->surface(),
+      connection_->seat(), connection_->serial(), bounds.x(), bounds.y()));
+  xdg_popup_add_listener(xdg_popup_.get(), &xdg_popup_listener, this);
+}
 
+// TODO(msisov, tonikitoo): we will want to trigger show and hide of all
+// windows once we pass minimize events from chrome.
+void WaylandWindow::Show() {
+  if (xdg_surface_)
+    return;
+  if (!xdg_popup_) {
+    CreatePopupWindow();
+    connection_->ScheduleFlush();
+  }
+}
+
+// TODO(msisov, tonikitoo): we will want to trigger show and hide of all
+// windows once we pass minimize events from chrome.
 void WaylandWindow::Hide() {
-  NOTIMPLEMENTED();
+  if (child_window_)
+    child_window_->Hide();
+  if (xdg_popup_)
+    xdg_popup_.reset();
 }
 
 void WaylandWindow::Close() {
@@ -111,11 +163,19 @@ void WaylandWindow::SetTitle(const base::string16& title) {
 }
 
 void WaylandWindow::SetCapture() {
-  NOTIMPLEMENTED();
+  if (g_current_capture_ == this)
+    return;
+
+  WaylandWindow* old_capture = g_current_capture_;
+  if (old_capture)
+    old_capture->delegate()->OnLostCapture();
+
+  g_current_capture_ = this;
 }
 
 void WaylandWindow::ReleaseCapture() {
-  NOTIMPLEMENTED();
+  if (g_current_capture_ == this)
+    g_current_capture_ = nullptr;
 }
 
 void WaylandWindow::ToggleFullscreen() {
@@ -159,18 +219,45 @@ PlatformImeController* WaylandWindow::GetPlatformImeController() {
 
 bool WaylandWindow::CanDispatchEvent(const PlatformEvent& native_event) {
   Event* event = static_cast<Event*>(native_event);
-  if (event->IsMouseEvent())
-    return has_pointer_focus_;
+  if (event->IsMouseEvent()) {
+    if (g_current_capture_ == this) {
+      if (has_pointer_focus_ || child_window_->is_focused_popup())
+        return true;
+      else
+        return false;
+    } else {
+      return has_pointer_focus_;
+    }
+  }
   if (event->IsKeyEvent())
     return has_keyboard_focus_;
   return false;
 }
 
 uint32_t WaylandWindow::DispatchEvent(const PlatformEvent& native_event) {
+  Event* event = static_cast<Event*>(native_event);
+  if (event->IsLocatedEvent() && !has_pointer_focus_)
+    ConvertEventLocationToCurrentWindowLocation(event);
+
   DispatchEventFromNativeUiEvent(
       native_event, base::Bind(&PlatformWindowDelegate::DispatchEvent,
                                base::Unretained(delegate_)));
   return POST_DISPATCH_STOP_PROPAGATION;
+}
+
+void WaylandWindow::ConvertEventLocationToCurrentWindowLocation(
+    ui::Event* event) {
+  WaylandWindow* wayland_window = connection_->GetCurrentFocusedWindow();
+  DCHECK_NE(wayland_window, this);
+  if (wayland_window && event->IsLocatedEvent()) {
+    gfx::Vector2d offset =
+        wayland_window->GetBounds().origin() - GetBounds().origin();
+    ui::LocatedEvent* located_event = event->AsLocatedEvent();
+    gfx::PointF location_in_pixel_in_host =
+        located_event->location_f() + gfx::Vector2dF(offset);
+    located_event->set_location_f(location_in_pixel_in_host);
+    located_event->set_root_location_f(location_in_pixel_in_host);
+  }
 }
 
 // static
@@ -181,6 +268,13 @@ void WaylandWindow::Configure(void* data,
                               wl_array* states,
                               uint32_t serial) {
   WaylandWindow* window = static_cast<WaylandWindow*>(data);
+
+  // Width or height set 0 means that we should decide on width and height by
+  // ourselves, but we don't want to set to anything else. Use previous size.
+  if (width == 0 || height == 0) {
+    width = window->GetBounds().width();
+    height = window->GetBounds().height();
+  }
 
   // Rather than call SetBounds here for every configure event, just save the
   // most recent bounds, and have WaylandConnection call ApplyPendingBounds
@@ -193,6 +287,13 @@ void WaylandWindow::Configure(void* data,
 // static
 void WaylandWindow::Close(void* data, xdg_surface* obj) {
   NOTIMPLEMENTED();
+}
+
+// static
+void WaylandWindow::PopupDone(void* data, xdg_popup* obj) {
+  WaylandWindow* window = static_cast<WaylandWindow*>(data);
+  window->Hide();
+  window->delegate_->OnCloseRequest();
 }
 
 }  // namespace ui
