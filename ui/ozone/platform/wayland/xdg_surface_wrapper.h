32b819f4459b6b13076627ff68a771279b4b2830
Maksim Sisov <msisov@igalia.com>
2017-07-05 15:00:09 +0300

    [ozone/wayland] Add xdg_v6 support
    This commit adds support for xdg_v6 by having a wrapper for different
    xdg shell versions.
    
    The interfaces xdg_popup_wrapper and xdg_surface_wrapper are
    implemented by different xdg objects with different versions.
    WaylandWindow doesn't know what version is used and relies on
    XDGShellObjectsFactory, which decides which version of objects
    to use by checking the shell version. What is more, this design
    should allow to add ivi shell support quite easily in the future.
    
    The decision of what xdg shell version to connect to is the following:
    when WaylandConnection::Global is called back,
    it checks what shell versions are available. The first version to
    be checked is V6. If V6 is not available, V5 is checked and
    the interface is binded. Otherwise V6 is used.

---
+++ ui/ozone/platform/wayland/xdg_surface_wrapper.h
@@ -0,0 +1,57 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_WAYLAND_XDG_SURFACE_WRAPPER_H_
+#define UI_OZONE_PLATFORM_WAYLAND_XDG_SURFACE_WRAPPER_H_
+
+#include "base/strings/string16.h"
+#include "ui/ozone/platform/wayland/wayland_object.h"
+
+namespace ui {
+
+class WaylandConnection;
+
+// Wrapper interface for different xdg shell versions.
+class XDGSurfaceWrapper {
+ public:
+  virtual ~XDGSurfaceWrapper() {}
+
+  // Initializes the surface. If |with_toplevel| is true, the surface is
+  // assigned a top level role, which results in a normal native window.
+  virtual bool Initialize(WaylandConnection* connection,
+                          wl_surface* surface,
+                          bool with_toplevel) = 0;
+
+  // Sets a native window to maximized state.
+  virtual void SetMaximized() = 0;
+
+  // Unsets a native window from maximized state.
+  virtual void UnSetMaximized() = 0;
+
+  // Sets a native window to fullscreen state.
+  virtual void SetFullScreen() = 0;
+
+  // Unsets a native window from fullscreen state.
+  virtual void UnSetFullScreen() = 0;
+
+  // Sets a native window to minimized state.
+  virtual void SetMinimized() = 0;
+
+  // Tells wayland to start interactive window drag.
+  virtual void SurfaceMove(WaylandConnection* connection) = 0;
+
+  // Tells wayland to start interactive window resize.
+  virtual void SurfaceResize(WaylandConnection* connection,
+                             uint32_t hittest) = 0;
+
+  // Sets a title of a native window.
+  virtual void SetTitle(const base::string16& title) = 0;
+
+  // Sends acknowledge configure event back to wayland.
+  virtual void AckConfigure() = 0;
+};
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_WAYLAND_XDG_SURFACE_WRAPPER_H_
