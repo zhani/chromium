bc10d719a5c9b070ac21565ae34d4301f715dc91
Maksim Sisov <msisov@igalia.com>
2017-08-21 14:29:23 +0300

    Propagate child-parent relationship from aura to mus.
    This commit simplifies the way how, for example, wayland
    makes a child-parent relationship. Instead of doing a fancy
    stuff with focuses (which involve races sometimes), it relies on
    the information propagated from aura and then finds the right parent
    for popup windows.
    
    fixup! Propagate child-parent relationship from aura to mus.
    
    Add check.
    
    fixup! Propagate child-parent relationship from aura to mus.
    
    Adapt to https://chromium-review.googlesource.com/c/chromium/src/+/617378.

--- services/ui/ws/window_tree.h
+++ services/ui/ws/window_tree.h
@@ -443,6 +443,9 @@ class WindowTree : public mojom::WindowTree,
   bool ProcessSetBlockingContainers(std::vector<mojom::BlockingContainersPtr>
                                         transport_all_blocking_containers);
 
+ // TODO(tonikitoo,msisov): Make these methods public in order to use them
+ // from WindowTreeHostFactory.
+ public:
   // Returns the ClientWindowId from a transport id or WindowId. Uses id_ as the
   // ClientWindowId::client_id part if it was invalid. These functions do a
   // straight mapping, there may not be a window with the returned id.
@@ -454,6 +457,7 @@ class WindowTree : public mojom::WindowTree,
   // that created the window.
   Id ClientWindowIdToTransportId(const ClientWindowId& client_window_id) const;
 
+private:
   // WindowTree:
   void NewWindow(uint32_t change_id,
                  Id transport_window_id,
